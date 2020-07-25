4afdb5e5559aed647b3816802894e6b02abd480d
camoverride <camoverride@gmail.com>
2020-07-24 23:59:57 -0700

    update tests

--- tests.py
+++ tests.py
@@ -3,7 +3,9 @@ from model import create_sentence, create_dict, clean_data, make_sentence
 
 # Test create_sentence
 
+
 # Test create_dict
+
 
 # Test clean_data
 messy_data = """
@@ -14,12 +16,5 @@ assert clean_data(messy_data) == "this test has a lot of junkkk that shou be rem
 
 
 # Test make_sentence
-my_string = """
-    1:2 And the !# earth was without $ form,
-    and void; and *()@ darkness was upon,
-    {  the } face of the deep. And the +  
-    Spirit of God moved upon the face of the = waters. - 1:3 And
-    God said, Let there be light: and there was light.
-    """
 
-assert make_sentence(my_string) == something
+
